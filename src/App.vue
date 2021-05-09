8cbecedb0c17bdc4fe156f0467fd0371c9a95684
Christian Vilen <c.vilen@outlook.com>
2021-05-09 17:15:08 +0300

    sort repos based on last updated and reduce margin on header image

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <b-container id="app">
     <b-row>
       <b-col>
-        <b-button variant="dark" class="color-btn" @click="changeTheme">Random color</b-button>
         <b-img class="header-img" src="./assets/mountain_logo.png" height="300" width="300" alt="Header image"
         ></b-img>
       </b-col>
@@ -27,55 +26,6 @@
     </b-row>
   </b-container>
 </template>
-
-<style>
-@import "assets/css/colors.css";
-@import "assets/css/global.css";
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  height: 100vh;
-}
-
-body, html {
-  background: var(--bg);
-  transition: background 500ms ease-in-out, color 1000ms ease-in-out;
-}
-
-.header {
-  margin-bottom: 2rem;
-}
-
-#footer {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-}
-
-.color-btn {
-  position: absolute;
-  top: 40px;
-  right: 40px;
-  font-size: 0.7rem;
-}
-
-.header-img {
-  margin-top: 7rem;
-}
-
-@media screen and (max-width: 678px) {
-  .header {
-    margin-bottom: 0;
-  }
-
-  .header-img {
-    margin-top: 5rem;
-  }
-}
-</style>
 
 <script>
 import Header from "@/components/Header";
@@ -110,3 +60,46 @@ export default {
   }
 }
 </script>
+
+
+<style>
+@import "assets/css/colors.css";
+@import "assets/css/global.css";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  height: 100vh;
+}
+
+body, html {
+  background: var(--bg);
+  transition: background 500ms ease-in-out, color 1000ms ease-in-out;
+}
+
+.header {
+  margin-bottom: 2rem;
+}
+
+#footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+
+.header-img {
+  margin-top: 2rem;
+}
+
+@media screen and (max-width: 678px) {
+  .header {
+    margin-bottom: 0;
+  }
+
+  .header-img {
+    margin-top: 1rem;
+  }
+}
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -5,15 +5,19 @@
         <b-card class="mb-4 shadow card">
           <b-card-title class="card-title" v-text="project.name"></b-card-title>
           <b-card-sub-title v-text="project.language"></b-card-sub-title>
-          <b-card-text class="body-text" v-text="project.description"></b-card-text>
+          <b-card-text
+            class="body-text"
+            v-text="project.description"
+          ></b-card-text>
           <b-row>
             <b-col>
               <b-link
-                  class="body-text"
-                  v-b-tooltip.hover.bottom
-                  title="Opens to Github.com"
-                  target="_blank"
-                  :href="project.html_url">View
+                class="body-text"
+                v-b-tooltip.hover.bottom
+                title="Opens to Github.com"
+                target="_blank"
+                :href="project.html_url"
+                >View
               </b-link>
             </b-col>
           </b-row>
@@ -22,40 +26,53 @@
     </b-row>
     <b-row class="mt-2 mb-2" v-if="projects.length > 0">
       <b-col>
-        <b-link href="https://github.com/ChristianVilen">/ChristianVilen</b-link>
+        <b-link href="https://github.com/ChristianVilen"
+          >/ChristianVilen</b-link
+        >
       </b-col>
     </b-row>
     <b-row v-if="projects.length > 0">
       <b-col>
-        <img class="octocat-img" src="../assets/Octocat.png" alt="Github Octocat">
+        <img
+          class="octocat-img"
+          src="../assets/Octocat.png"
+          alt="Github Octocat"
+        />
       </b-col>
     </b-row>
   </main>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "Portfolio",
   data() {
     return {
       projects: [],
-    }
+    };
   },
   mounted() {
-    this.getRepos()
+    this.getRepos();
   },
   methods: {
     getRepos() {
-      axios.get('https://api.github.com/users/ChristianVilen/repos')
-          .then(r => {
-            this.projects = r.data.reverse()
-          })
-          .catch(e => console.log(e))
-    }
-  }
-}
+      axios
+        .get("https://api.github.com/users/ChristianVilen/repos")
+        .then((r) => {
+          this.sortRepos(r.data);
+        })
+        .catch((e) => console.log(e));
+    },
+    sortRepos(data) {
+      data.sort((a, b) => {
+        return new Date(b.updated_at) - new Date(a.updated_at);
+      });
+      this.projects = data
+    },
+  },
+};
 </script>
 
 <style scoped>
